0f0e2c77b8750b77c9c45370ca18cbef2309a161
RIstiaque <rifatul.istiaque@gmail.com>
2019-01-09 12:22:52 -0500

    added test for get method

--- server/server.js
+++ server/server.js
@@ -5,35 +5,6 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
-// var newTodo = new Todo({
-//     text: 'Cook dinner'
-// });
-
-// newTodo.save().then((doc) => {
-//     console.log('Saved todo', doc);
-// }, (e) => {
-//     console.log('Unable to save.');
-// });
-
-// var nexTodo = new Todo({
-//     text: '    Edit this video    '
-// });
-// nexTodo.save().then((result) => {
-//     console.log(`Saved todo:\n ${result}`);
-// }, (e) => {
-//     console.log('Unable to save');
-// })
-
-// var newUser = new User({
-//     email: 'andrew@udemy.com'
-// });
-
-// newUser.save().then((result) => {
-//     console.log(JSON.stringify(result, undefined, 2));
-// }, (e) => {
-//     console.log('Unable to save User: ', e);
-// });
-
 var app = express();
 
 app.use(bodyParser.json());
