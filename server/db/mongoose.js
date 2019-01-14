6150c268814c6683c6faba8d836afeaf3d75d422
RIstiaque <rifatul.istiaque@gmail.com>
2019-01-14 14:12:28 -0500

    heroku deployment

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,5 +2,6 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://spame:Algolia12!@ds257314.mlab.com:57314/somedb');
 
 module.exports = {mongoose};
