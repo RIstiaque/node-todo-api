59dd0e4536ed3201d68c9094cb432d356c2e2cd0
RIstiaque <rifatul.istiaque@gmail.com>
2019-01-17 20:55:49 -0500

    Added test database

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 // mongoose.connect('mongodb://spame:Algolia12!@ds257314.mlab.com:57314/somedb');
 
 module.exports = {mongoose};
